8097390f88ec6498c959df2ff62960774836b1f8
gaoyi <577981827@qq.com>
2021-08-04 20:50:57 +0800

    feat(join): create game join view & html static files to be rendered ✅

---
+++ monopoly/routing.py
@@ -0,0 +1,10 @@
+from django.urls import re_path
+
+from .consumers.join import JoinConsumer
+from .consumers.game import GameConsumer
+
+
+websocket_urlpatterns = [
+    re_path(r'ws/join/(?P<room_name>\w+)/$', JoinConsumer.as_asgi()),
+    re_path(r'ws/game/(?P<game_id>\w+)/$', GameConsumer.as_asgi()),
+]
--- monopoly/urls.py
+++ monopoly/urls.py
@@ -5,6 +5,10 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
+    path('join/', login_required(JoinView.as_view()), name="join"),
+    path('join/<str:host_name>/', login_required(JoinView.as_view()), name="join"),
+    path('game/<str:host_name>/', login_required(GameView.as_view()), name='game'),
+
     path('', LoginView.as_view(), name='login'),
     path('register/', RegisterView.as_view(), name='register'),
     path('profile/<str:username>/', login_required(ProfileView.as_view()), name='profile'),
