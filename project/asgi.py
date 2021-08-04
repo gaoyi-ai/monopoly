dd97f55f4dd0a10c0c9e1fee0a2867e00d553be7
gaoyi <577981827@qq.com>
2021-08-04 21:18:13 +0800

    fix(asgi): fix asgi implementation miss ❎

--- project/asgi.py
+++ project/asgi.py
@@ -13,16 +13,15 @@ from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
 django_asgi_app = get_asgi_application()
 
 import monopoly.routing
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
-    "websocket": AuthMiddlewareStack(
-        URLRouter(
-            monopoly.routing.websocket_urlpatterns
-        )
-    ),
+    # "websocket": AuthMiddlewareStack(
+    #     URLRouter(
+    #         monopoly.routing.websocket_urlpatterns
+    #     )
+    # ),
 })
