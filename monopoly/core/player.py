e13d2ec9bea41d30bcf275ec7859a8b9a1c36b04
gaoyi <577981827@qq.com>
2021-08-10 00:59:27 +0800

    feat(core): game logic core function ✅

---
+++ monopoly/core/player.py
@@ -0,0 +1,76 @@
+from monopoly.core.util import *
+
+
+class Player:
+
+    def __init__(self, index):
+        self._index = index
+        self._money = INIT_PLAYER_MONEY
+        self._position = 0
+        self._remaining_stop = 0  # 等待回合数
+        self._properties = set()
+
+    def get_position(self):
+        return self._position
+
+    def get_money(self):
+        return self._money
+
+    def get_stop(self):
+        return self._remaining_stop
+
+    def add_properties(self, land):
+        self._properties.add(land)
+
+    def get_properties(self):
+        return self._properties
+
+    def get_asset(self):
+        ret = self.get_money()
+        for land in self._properties:
+            ret += land.get_evaluation()
+        return ret
+
+    def remove_property(self, building):
+        self._properties.remove(building)
+
+    def set_money(self, new_val):
+        self._money = new_val
+
+    def add_money(self, val):
+        self._money += val
+
+    def deduct_money(self, val):
+        self._money -= val
+
+    def get_stop_num(self):
+        return self._remaining_stop
+
+    def set_stop(self, val):
+        self._remaining_stop = val
+
+    def add_stop(self, val):
+        self._remaining_stop += val
+
+    def add_one_stop(self):
+        self._remaining_stop += 1
+
+    def deduct_stop_num(self):
+        self._remaining_stop -= 1
+
+    def set_position(self, pos):
+        self._position = pos
+
+    def get_index(self):
+        return self._index
+
+    def __str__(self):
+        return "Player index: {0}".format(self._index)
+
+
+def test():
+    pass
+
+
+if __name__ == '__main__':
+    test()
