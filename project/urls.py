a45b9f610820553f9f1e6d3d77da07d6f406026e
gaoyi <577981827@qq.com>
2021-08-15 00:01:22 +0800

    fix(url): fix issue:#1

--- project/urls.py
+++ project/urls.py
@@ -13,10 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
+from project.settings.base import MEDIA_URL, MEDIA_ROOT
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('monopoly/', include('monopoly.urls')),
-]
+    path('api/', include('monopoly.api.urls')),
+] + static(MEDIA_URL, document_root=MEDIA_ROOT)
