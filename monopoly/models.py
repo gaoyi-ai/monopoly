aa47b9f18493797c4cd7691f86b87a0b2b6e5015
gaoyi <577981827@qq.com>
2021-08-15 15:28:19 +0800

    perf(url): optimize database model and url ✅

--- monopoly/models.py
+++ monopoly/models.py
@@ -1,5 +1,14 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from rest_framework.authtoken.models import Token
+
+
+@receiver(post_save, sender=User)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    if created:
+        Token.objects.create(user=instance)
 
 
 def user_directory_path(instance, filename):
@@ -8,11 +17,11 @@ def user_directory_path(instance, filename):
 
 # use the default User model and this Profile model to represent user info
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, related_name='profile', on_delete=models.CASCADE)
     avatar = models.FileField(upload_to=user_directory_path, blank=True, null=True)
 
     def __str__(self):
-        return str(self.user)
+        return f"Profile [id:{self.id}, user_id:{self.user.id}, username:{self.user.username}]"
 
     class Meta:
         app_label = "monopoly"
