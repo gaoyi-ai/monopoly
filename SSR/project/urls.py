115f9e6fde171fd151220749ff92e71d34701d5b
Haoyu-Wang-19023462 <vista949698420@foxmail.com>
2021-10-12 16:20:56 +0800

    Move tests.py and generate an HTML test report.

--- SSR/project/urls.py
+++ SSR/project/urls.py
@@ -3,10 +3,15 @@ from django.contrib import admin
 from django.urls import include, path
 from project.settings.base import MEDIA_URL, MEDIA_ROOT
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
+from django.conf import settings
+from django.conf.urls import url
+from django.views import static as static_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('monopoly/', include('monopoly.urls')),
+    url(r'^static/(?P<path>.*)$', static_view.serve,
+        {'document_root': settings.STATIC_ROOT}, name='static'),
     path('api/', include('monopoly.api.urls')),
     # API PATTERNS
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
